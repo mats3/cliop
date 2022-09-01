ce88ecfbf9843a468a40e61338450ac70e29d1e0
mats3 <faktorx24@gmail.com>
2022-09-01 23:22:18 +0200

    cliop.h added version number major, minor, patch

--- src/cliop/cliop.h
+++ src/cliop/cliop.h
@@ -1,6 +1,10 @@
 #ifndef CLIOP_H
 #define CLIOP_H
 
+#define VERS_CLIOP_MAJOR 0
+#define VERS_CLIOP_MINOR 0
+#define VERS_CLIOP_PATCH 0
+
 #include"types.h"
 
 void cliop_option_append(char option, char *description, void *exec_func); /* append a option to Global Options */
